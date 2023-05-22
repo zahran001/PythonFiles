f8fd4af396b9d1e0fb4077536f54cb908b39cb36
zahran001 <114562123+zahran001@users.noreply.github.com>
2023-05-23 00:39:58 +0600

    Update and rename Problem 3.py to Slope_of_a_line.py

--- Slope_of_a_line.py
+++ Slope_of_a_line.py
@@ -1,12 +1,7 @@
-#Zahran Yahia Khan
-#U63179657
 #The program calculates the slope of a line after taking inputs in the required way.
 
 x1,y1 = input('Enter the coordinates of the first point:').split()
 x2,y2 = input('Enter the coordinates of the second point:').split()
-
-#As per the given output, we should specify the style of providing input here. For example,
-#x1,y1 = input('Enter the coordinates of the first point (separated by spaces):').split()
 
 x1 = float(x1)
 y1= float(y1)
