19a29ad62a9aa5ebc78a33510632edea7c8d1bf7
zahran001 <114562123+zahran001@users.noreply.github.com>
2023-05-23 01:02:34 +0600

    Add files via upload

---
+++ Problem 1.py
@@ -0,0 +1,85 @@
+#Zahran Yahia Khan
+#U63179657
+#This program has N-sided Polygon class definition and main function
+
+# Data hiding in Python is done by using a double underscore before (prefix) the attribute name.
+class Polygon:
+    def __init__(self): #initializer method
+        self.__sides = 0
+        self.__length = 0.0
+
+    #accessor methods - get data
+
+    def getsides(self):
+        return self.__sides
+
+    def getlength(self):
+        return self.__length
+
+    #mutator methods - update data
+
+    def setsides(self,n):
+        self.__sides = n
+
+    def setlength(self,s):
+        self.__length = s
+
+    #other methods
+
+    def perimeter(self):
+        return (self.__sides * self.__length)
+
+    def area(self):
+        import math
+        pi = math.pi
+        t = math.tan(pi / self.__sides)
+        numerator =  (self.__sides * (self.__length**2))
+        denominator = 4 * t
+        area = numerator/denominator
+        return area
+
+
+
+#main function
+def main():
+    p1 = Polygon() #created a object
+
+    #creating local variables
+    NoOfSides = int(input('Enter the number of sides (>=3): '))
+    while NoOfSides < 3:
+        NoOfSides = int(input('Invalid entry. Re-enter the number of sides (>=3): '))
+
+    LenEachSide = float(input('Enter the length of each sides (>= 0.1): '))
+    while LenEachSide < 0.1:
+        LenEachSide = float(input('Invalid entry. Re-enter the length of each sides (>= 0.1): '))
+
+    p1.setsides(NoOfSides)
+    p1.setlength(LenEachSide)
+
+    print(f'The polygon has {p1.getsides()} sides. Each side is {p1.getlength()} units in length.')
+    print(f'The perimeter of the polygon is {p1.perimeter():.3f} units and its area is {p1.area():.3f} square units.')
+
+
+
+#call
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Problem 2.py
@@ -0,0 +1,41 @@
+#Zahran Yahia Khan
+#U63179657
+#This program simulates the Retail items problem
+
+class Retail_Item:
+    def __init__(self, t, a, p):
+        self.__TypeOfItem = t
+        self.__amount = a
+        self.__price = p
+
+    def __str__(self):
+        return f"{self.__TypeOfItem.ljust(10)}{self.__amount.center(10)}{('$' + self.__price).rjust(10)}" #using an f-string
+
+def main():
+    name1 = input('Name of item 1: ')
+    amount1 = input('Amount of item 1: ')
+    price1 = input('Price of item 1: ')
+
+
+
+    name2 = input('Name of item 2: ')
+    amount2 = input('Amount of item 2: ')
+    price2 = input('Price of item 2: ')
+
+    obj1 = Retail_Item(name1, amount1, price1)
+    obj2 = Retail_Item(name2, amount2, price2)
+
+    print('Here is a summary of the items you added:')
+
+    print('Item'.ljust(10) + 'Amount'.center(10) + 'Price'.rjust(10))
+    print('-' * 30)
+
+    print(obj1)
+    print(obj2)
+
+
+
+
+#call
+main()
+
