c32281a307e12b5d1084022b8b3db014706d5c2e
zahran001 <114562123+zahran001@users.noreply.github.com>
2023-05-23 00:48:23 +0600

    Update and rename Problem 4.py to Ratio_Calculator.py

--- Ratio_Calculator.py
+++ Ratio_Calculator.py
@@ -1,5 +1,3 @@
-#Zahran Yahia Khan
-#U63179657
 #The program calculates the ratio of volume between the spheres and the container using appropriate formulas.
 
 #math module is used for this calculation
@@ -14,8 +12,6 @@ vol_spheres = no0Fspheres * 4/3* pi * r**3
 height_cylinder = no0Fspheres * diameter_each_sphere
 vol_cylinder = pi * r * r * height_cylinder
 print(f'The container would need to be at least {height_cylinder} inches to hold {no0Fspheres} spheres.')
-#The volume occupied by the spheres in the container:
-# V= volume occupied by the spheres in the container
 V = (vol_spheres/vol_cylinder) * 100
 print(f'The {no0Fspheres} spheres will occupy {V:.2f}% of the container.')
 
