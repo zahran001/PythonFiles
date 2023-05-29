deda459c90f6b0f46720d8d057922fa6e0be2578
zahran001 <114562123+zahran001@users.noreply.github.com>
2023-05-29 13:37:53 +0600

    Update and rename My adventure game.py to Adventure_game.py

--- Adventure_game.py
+++ Adventure_game.py
@@ -1,4 +1,4 @@
-#My own adventure game
+#Adventure game
 name = input("Type your name: ")
 print("Welcome",name,"to this adventure")
 answer = input("You are on a dirt road, it has come to an end and you can go left or right. Which way would you like to go?: ").lower()
